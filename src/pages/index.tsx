9025d7dd603f97ed5d497ef370899a9ab4e25f64
Mikhail Balin <m.balin@icloud.com>
2023-03-07 12:00:56 +0700

    update fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import { Accordion } from "@mantine/core";
 
 const inter = Inter({ subsets: ["latin"] });
