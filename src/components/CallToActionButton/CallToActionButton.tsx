261c6f5ac7e11176ea023e68577b985173936628
Mikhail Balin <m.balin@icloud.com>
2023-04-07 11:47:57 +0700

    update CTA button

--- src/components/CallToActionButton/CallToActionButton.tsx
+++ src/components/CallToActionButton/CallToActionButton.tsx
@@ -8,21 +8,21 @@ export const CallToActionButton = () => (
     href="https://twitter.com/mantinedev"
     styles={(theme) => ({
       root: {
-        backgroundColor: "#00acee",
-        border: 0,
-        height: rem(42),
-        paddingLeft: rem(20),
-        paddingRight: rem(20),
-        "&:not([data-disabled])": theme.fn.hover({
-          backgroundColor: theme.fn.darken("#00acee", 0.05),
-        }),
-      },
-
-      leftIcon: {
-        marginRight: theme.spacing.md,
+        border: "1px solid #000",
+        borderRadius: "50%",
+        color: "#000",
+        textTransform: "none",
+        // backgroundColor: "#00acee",
+        // border: 0,
+        // height: rem(42),
+        // paddingLeft: rem(20),
+        // paddingRight: rem(20),
+        // "&:not([data-disabled])": theme.fn.hover({
+        //   backgroundColor: theme.fn.darken("#00acee", 0.05),
+        // }),
       },
     })}
   >
-    Follow on Twitter
+    Book a Call
   </Button>
 );
